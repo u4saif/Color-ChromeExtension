b4dfe0f6c575b3102b628aa30bc342316ef19b26
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-11-26 00:40:33 +0530

    rgb converter added

--- assets/js/appScript.js
+++ assets/js/appScript.js
@@ -1,16 +1,24 @@
 const hexRegex=/[0-9A-Fa-f]{6}/g;
 
 function colorPicked(e){ 
-    let value=e.target.value.split('#')[1];
-    document.getElementById("hexValue").value=value;
+    let rgbValue=convertHexToRGBA(e.target.value);
+    let hexValue=e.target.value.replace("#", "");
+
+    document.getElementById("hexValue").value=hexValue;
     document.getElementById('hexValue').style.background="white";
     document.getElementById('hexValue').style.color="black";
+
+    document.getElementById("rvalue").value=rgbValue[0];
+    document.getElementById("gvalue").value=rgbValue[1];
+    document.getElementById("bvalue").value=rgbValue[2];
+
+
 }
 
 var copyToClip=function(id){
-	let copy_text_val= document.getElementById(id)
-    copy_text_val.select();
-    document.execCommand("copy");
+	let copy_text_val= document.getElementById(id).select();
+    let val='#'+copy_text_val;
+    document.execCommand("copy",val);
 }
 
 var colorInputChanged=function(e){
@@ -25,6 +33,20 @@ var colorInputChanged=function(e){
     }
 }
 
+// Convertor functions
+
+function convertHexToRGBA(colorHex) {
+    let hex = colorHex.replace("#", "");
+    if (hex.length === 3) {
+      hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`;
+    }
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+  
+    return [r,g,b];
+  }
+  
 document.getElementById("colorPicker")?.addEventListener('input',colorPicked)
 document.getElementById("hexValue")?.addEventListener('input',colorInputChanged)
 document.getElementById("hexCopyIcon")?.addEventListener('click',copyToClip.bind(event,'hexValue'));
