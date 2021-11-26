74ce92964707f5e3da4bca1312acc9225822279f
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-11-26 21:50:26 +0530

    hex to hs convert

--- assets/js/appScript.js
+++ assets/js/appScript.js
@@ -16,7 +16,56 @@ function convertHexToRGBA(colorHex) {
 
 function rgbTohex(r, g, b) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  }  
+  } 
+
+function HEXtoHSL(hex) {
+    hex = hex.replace(/#/g, '');
+    if (hex.length === 3) {
+        hex = hex.split('').map(function (hex) {
+            return hex + hex;
+        }).join('');
+    }
+    var result = /^([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})[\da-z]{0,0}$/i.exec(hex);
+    if (!result) {
+        return null;
+    }
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b),
+        min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+    if (max == min) {
+        h = s = 0;
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+        case r:
+            h = (g - b) / d + (g < b ? 6 : 0);
+            break;
+        case g:
+            h = (b - r) / d + 2;
+            break;
+        case b:
+            h = (r - g) / d + 4;
+            break;
+        }
+        h /= 6;
+    }
+    s = s * 100;
+    s = Math.round(s);
+    l = l * 100;
+    l = Math.round(l);
+    h = Math.round(360 * h);
+
+    return {
+        h: h,
+        s: s,
+        l: l
+    };
+}
 //Calling Required Functions
 
 var copyToClip=function(id){
@@ -28,8 +77,8 @@ var copyToClip=function(id){
 //Hex Input Event Handelers 
 function colorPicked(e){ 
     let rgbValue=convertHexToRGBA(e.target.value);
+    let hslValue=HEXtoHSL(e.target.value)
     let hexValue=e.target.value.replace("#", "");
-
     document.getElementById("hexValue").value=hexValue;
     document.getElementById('hexValue').style.background="white";
     document.getElementById('hexValue').style.color="black";
@@ -38,17 +87,27 @@ function colorPicked(e){
     document.getElementById("gvalue").value=rgbValue[1];
     document.getElementById("bvalue").value=rgbValue[2];
 
+    document.getElementById("hvalue").value=hslValue["h"];
+    document.getElementById("svalue").value=hslValue["s"];
+    document.getElementById("lvalue").value=hslValue["l"];
+
+
 
 }
 
 var colorInputChanged=function(e){
     if(hexRegex.test(e.target.value)) {
         let rgbValue=convertHexToRGBA(e.target.value);
+        let hslValue=HEXtoHSL(e.target.value)
         let value='#'+e.target.value;
         document.getElementById("colorPicker").value=value;
         document.getElementById("rvalue").value=rgbValue[0];
         document.getElementById("gvalue").value=rgbValue[1];
         document.getElementById("bvalue").value=rgbValue[2];
+
+        document.getElementById("hvalue").value=hslValue["h"];
+        document.getElementById("svalue").value=hslValue["s"];
+        document.getElementById("lvalue").value=hslValue["l"];
 
         document.getElementById('hexValue').style.background="white";
         document.getElementById('hexValue').style.color="black";
